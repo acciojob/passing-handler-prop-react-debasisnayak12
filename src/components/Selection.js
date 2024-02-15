46c2ba42286cad0e116af2e7fc6de25601492ee7
Debasis Nayak <debasishnayak0739@gmail.com>
2024-02-15 12:17:39 +0530

    done

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { useState } from "react";
 
+const Selection = ({ applyColor }) => {
+const[bgColor, setBgColor] = useState("");
+
+const changeColor = () => {
+    applyColor((background) => {
+      setBgColor(background);
+    });
+}
+
+    return (
+        <div className="fix-box" style={{backgroundColor: bgColor.background,width:200,height:200,border:"1px solid gray"}} onClick={changeColor}>
+            <p>Selection</p>
+        </div>
+    )
+}
+
+export default Selection;
